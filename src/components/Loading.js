6dea5df2f521eb856c75a2270582306d128c30c2
beyzanurakin <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>
2022-06-30 15:47:15 +0300

    configure tailwind file

---
+++ src/components/Loading.js
@@ -0,0 +1,13 @@
+const Loading = () => {
+  let circleCommonClasses = 'h-2.5 w-2.5 bg-current rounded-full'
+
+  return (
+    <div className='flex'>
+      <div className={`${circleCommonClasses} mr-1`}></div>
+      <div className={`${circleCommonClasses} mr-1`}></div>
+      <div className={`${circleCommonClasses}`}></div>
+    </div>
+  )
+}
+
+export default Loading
