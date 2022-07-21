f59659715719f5ec2371787105cfc3d7c275fe34
beyzanurakin <akinbeyzanur@gmail.com>
2022-07-21 18:28:33 +0300

    Add Dropdown to Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,36 +1,76 @@
 import React, { useState } from 'react'
-import { Button, Center, Flex, Icon, Input, useToast } from '@chakra-ui/react'
+import {
+  Button,
+  Center,
+  Flex,
+  Icon,
+  Input,
+  useToast,
+  Select,
+} from '@chakra-ui/react'
 import { useDispatch } from 'react-redux'
 import { getWeatherByCity, getWeatherByLocation } from '../redux/actions'
 import { HiLocationMarker } from 'react-icons/hi'
+import cityjson from '../cities.json'
 
 function Navbar() {
   const [city, setCity] = useState('')
+  const [cities, setCities] = useState(cityjson.data)
   const dispatch = useDispatch()
   const toast = useToast()
   const handleChange = () => {
     dispatch(getWeatherByCity(city, toast))
   }
+  console.log(cities)
 
   const handleLocationData = () => {
     dispatch(getWeatherByLocation(toast))
   }
+  const handleSelectChange = (e) => {
+    let selectedCity = e.target.value
+    dispatch(getWeatherByCity(selectedCity, toast))
+    console.log(selectedCity.toLowerCase())
+    // dispatch(getWeatherByCity())
+  }
   return (
-    <Flex>
+    <Flex
+      p={'10px'}
+      minH={'70px'}
+      bg={'#d7defa'}
+      justifyContent={'center'}
+      flexDirection={['column', 'row']}
+      gap={['10px', '20px']}
+    >
+      <Center>
+        <Select
+          onChange={handleSelectChange}
+          placeholder='Select city'
+          borderRadius={'15px'}
+          bg={'white'}
+        >
+          {cities.map((c, i) => {
+            return (
+              <option key={c.id} value={c.name}>
+                {c.name.toLowerCase()}
+              </option>
+            )
+          })}
+        </Select>
+      </Center>
       <Center>
         <Input
           onInput={(e) => {
             setCity(e.target.value)
           }}
           value={city}
-          borderRadius={'15px 0px  0px 15px'}
+          borderRadius={'15px'}
           bg={'white'}
           _focus={{ border: 'none' }}
           placeholder='city'
         ></Input>
         <Button
           onClick={handleChange}
-          borderRadius={'0px 15px 0px 15px'}
+          borderRadius={'15px'}
           color={'white'}
           bg={'#5e83f4'}
           _hover={{ bg: '5e82f4' }}
